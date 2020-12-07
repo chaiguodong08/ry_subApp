7e6b77f4392cdac6e7808cc1e412c7da71f6b033
chai <18510281247@163.com>
2020-12-07 13:48:52 +0800

    增加一个人View 路由映射路径。

---
+++ src/views/test/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    222222222200000000
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "index"
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
