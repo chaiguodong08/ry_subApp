cff0f2309d95d3272a31913e2d572b3902df4909
chai <18510281247@163.com>
2020-12-08 13:20:19 +0800

    增加占位文件，加载后可删除。

---
+++ src/util/test/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    1234567890
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "index"
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
